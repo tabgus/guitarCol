6c547752f95c447df22cab6eef7190ba1de95a5c
devgus <tabcorg@gmail.com>
2023-02-23 05:46:45 -0500

    header.css actualizado

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/footer.module.css";
 export default function Footer() {
@@ -9,6 +10,16 @@ export default function Footer() {
           <Link href="/nosotros">Nosotros</Link>
           <Link href="/blog">Blog</Link>
           <Link href="/tienda">Tienda</Link>
+          <Link legacyBehavior href="/carrito">
+            <a>
+              <Image
+                width={30}
+                height={25}
+                src="/img/carrito.png"
+                alt="imagen carrito"
+              />
+            </a>
+          </Link>
         </nav>
         <p className={styles.derechos}>
           Todos los derechos reservados {new Date().getFullYear()}
